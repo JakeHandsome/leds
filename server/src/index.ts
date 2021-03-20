20d51f8032591ad8ea1a76e5bf94e04f6c493370
Jake Hansen <JakeMHansen95@gmail.com>
2021-03-20 15:36:50 -0500

    Draft of colors, need to test on pi

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,47 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { exec as execNotAPromiseSoItsDumb } from "child_process";
+import { promisify } from "util";
+const exec = promisify(execNotAPromiseSoItsDumb);
+interface changecolor {
+   color: string;
+}
+
+interface Color {
+   Red: number;
+   Green: number;
+   Blue: number;
+}
+
+const colorToInts = (color: string): Color => {
+   return {
+      Red: parseInt(`0x${color.slice(1, 3)}`),
+      Green: parseInt(`0x${color.slice(3, 5)}`),
+      Blue: parseInt(`0x${color.slice(5, 7)}`),
+   };
+};
 
 const main = async () => {
-  const app = express();
-
-  app.get("/api", (_, res) => {
-    res.json({ poop: ":)" });
-  });
-  app.listen(8000, () => {
-    console.log("server.started on localhost:8000");
-  });
+   const app = express();
+   var jsonParser = bodyParser.json();
+   app.get("/api", (_, res) => {
+      res.json({ poop: ":)" });
+   });
+   app.listen(8000, () => {
+      console.log("server.started on localhost:8000");
+   });
+   app.post("/api/changecolor", jsonParser, (req, res) => {
+      const data: changecolor = req.body;
+      console.log(data.color);
+      const color = colorToInts(data.color);
+      exec(
+         `python3 ../pytest/test.py ${color.Red} ${color.Blue} ${color.Green}`
+      ).then(() => {
+         res.json({ success: true });
+      });
+   });
 };
 
 main().catch((err) => {
-  console.error(err);
+   console.error(err);
 });
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,18 +2,36 @@ import React, { useState } from "react";
 
 function App() {
    const [data, setData] = useState<string>("data");
+   const [color, setColor] = useState<string>("#FFFFFF");
    const callBackEnd = async () => {
       fetch("/api")
          .then((res) => res.json())
          .then((d) => setData(data + JSON.stringify(d, null, 2)));
    };
+   const handleChange = (event: any) => {
+      console.log({ event });
+      console.log(event.target.value);
+      setColor(event.target.value);
+   };
+
+   const submit = () => {
+      const requestOptions: RequestInit = {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({ color: color }),
+      };
+      fetch("/api/changecolor", requestOptions)
+         .then((response) => response.json())
+         .then((data) => console.log(data));
+   };
    return (
       <div className="App">
-         I want to die <button onClick={callBackEnd}>I am a dumb button</button>
+         I want to die
+         <button onClick={callBackEnd}>I am a dumb button</button>
          <p>{data}</p>
-         <form>
-            <label> Red </label>
-         </form>
+         <label>Color</label>
+         <input type="color" value={color} onChange={handleChange} />
+         <button onClick={submit}>Change color</button>
       </div>
    );
 }
