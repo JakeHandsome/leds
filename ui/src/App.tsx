44d9a9074413bd016e96a9197721f32558f9b492
Jake Hansen <Jakemhandsome@gmail.com>
2021-03-20 15:54:53 +0000

    Editor config and other stuff

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  const [data, setData] = useState<string>("data");
-  const callBackEnd = async () => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((d) => setData(data + "\n" + JSON.stringify(d, null, 2)));
-  };
-  return (
-    <div className="App">
-      I want to die <button onClick={callBackEnd}>I am a dumb button</button>
-      <p>{data}</p>
-    </div>
-  );
+   const [data, setData] = useState<string>("data");
+   const callBackEnd = async () => {
+      fetch("/api")
+         .then((res) => res.json())
+         .then((d) => setData(data + JSON.stringify(d, null, 2)));
+   };
+   return (
+      <div className="App">
+         I want to die <button onClick={callBackEnd}>I am a dumb button</button>
+         <p>{data}</p>
+      </div>
+   );
 }
 
 export default App;
