fe643af90cb19f0257ff2352a47c235aacc44350
Jake Hansen <JakeMHansen95@gmail.com>
2021-03-21 19:07:20 -0500

    Allow compile to windows to test non PI related items

--- server/IoT/LEDDriver.cs
+++ server/IoT/LEDDriver.cs
@@ -1,18 +1,23 @@
 ﻿using System.Threading;
 using System;
+#if LINUX_ARM
 using System.Device.Spi;
 using Iot.Device.Ws28xx;
+#endif
 using System.Drawing;
 
 namespace IoT
 {
    public class LEDDriver
    {
+#if LINUX_ARM
       private Ws28xx lights;
       private SpiDevice spi;
+#endif
       public int Count { private set; get; }
       public LEDDriver(int count)
       {
+#if LINUX_ARM
          Count = count;
          var settings = new SpiConnectionSettings(0, 0)
          {
@@ -22,15 +27,18 @@ namespace IoT
          };
          spi = SpiDevice.Create(settings);
          lights = new Ws2812b(spi, Count);
+#endif
       }
       public void SetAllLights(Color color)
       {
+#if LINUX_ARM
          var image = lights.Image;
          for (var i = 0; i < Count; i++)
          {
             image.SetPixel(i, 0, color);
          }
          lights.Update();
+#endif
       }
    }
 }
