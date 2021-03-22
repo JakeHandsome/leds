31b540498a22b1c42ac3e4ddfc7a4f6a6e6ca998
Jake Hansen <JakeMHansen95@gmail.com>
2021-03-21 21:22:27 -0500

    Delete stupid python script

--- pytest/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import board
-import neopixel
-pixels = neopixel.NeoPixel(board.D18, 150)
-
-if len(sys.argv) == 4:
-    pixels.fill((int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])))
