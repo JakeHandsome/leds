92737537ace2b3c60f81d76f2ec9e833eba88d0f
Jake Hansen <JakeMHansen95@gmail.com>
2021-04-25 09:26:47 -0500

    Using Singleton pattern for the console

--- ESP32/include/screen.h
+++ ESP32/include/screen.h
@@ -6,30 +6,32 @@
 #define TERMINAL_WIDTH 25
 #define TERMINAL_HEIGHT 8
 
+
 #define base U8G2_SSD1306_128X64_NONAME_F_HW_I2C
 class Screen : public base
 {
+   private:
+      uint8_t buf[TERMINAL_HEIGHT * TERMINAL_WIDTH];
+      Screen() : base(U8G2_R2, RST_OLED, SCL_OLED, SDA_OLED) {}
+      // Stop compiler generating methods to copy object
+      Screen(Screen const &copy);
+      Screen &operator=(Screen const &copy);
+
    public:
       U8G2LOG log;
-      uint8_t buf[TERMINAL_HEIGHT * TERMINAL_WIDTH];
-      int LineHeight;
-      int MaxWidth;
-      int MaxHeight;
-      int MaxLines;
-      Screen() : base(U8G2_R2, RST_OLED, SCL_OLED, SDA_OLED)
-      {
-      }
       void begin()
       {
          base::begin();
          setFont(u8g2_font_profont10_tf);
-         LineHeight = getFontAscent() - getFontDescent();
-         MaxHeight = getHeight();
-         MaxWidth = getWidth();
          log.begin((class U8G2&) *this, TERMINAL_WIDTH, TERMINAL_HEIGHT, buf);
 
          log.println("Screen init");
       }
+      static Screen& GetInstance()
+      {
+         static Screen s;
+         return s;
+      }
 };
 #undef base
-#endif
+#endif // SCREEN_H
